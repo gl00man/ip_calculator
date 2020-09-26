6d4df84e232c73ed926f0f22ab12ae6a823b3a3c
gl00man <maciekbereda46@gmail.com>
2020-09-26 22:20:29 +0200

    atomated push ~moje lenistwo dosiegnelo tego stopnia

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function Main()
 {
-  var ip = document.getElementById('ipAddress').value;
+  var input = document.getElementById('ipAddress').value
+  if(input != " " && input != "")
+  {
+  FillIpAdress(input);
+  FillMask(input);
+  }
+}
+
+function FillIpAdress(ip)
+{
   var address = ip.split(".");
   var fullAddr = []
   for(i = 0; i < address.length; i++)
@@ -10,6 +19,31 @@ function Main()
   var ready = (fullAddr.join(".")).replace(",", "");
   document.getElementById('decAddressTh').innerHTML = ip;
   document.getElementById('binAddressTh').innerHTML = ready;
+}
+
+function FillMask(ip)
+{
+  var ip = document.getElementById('ipAddress').value;
+  if(Number(ip.split(".")[0]) >= 1 && Number(ip.split(".")[0]) <= 126)
+  {
+    document.getElementById('decMaskTh').innerHTML = "255.0.0.0";
+    document.getElementById('binMaskTh').innerHTML = "11111111.0.0.0";
+  }
+  else if(Number(ip.split(".")[0]) >= 128 && Number(ip.split(".")[0]) <= 191)
+  {
+    document.getElementById('decMaskTh').innerHTML = "255.255.0.0";
+    document.getElementById('binMaskTh').innerHTML = "11111111.11111111.0.0";
+  }
+  else if(Number(ip.split(".")[0]) >= 192 && Number(ip.split(".")[0]) <= 223)
+  {
+    document.getElementById('decMaskTh').innerHTML = "255.255.255.0";
+    document.getElementById('binMaskTh').innerHTML = "11111111.11111111.11111111.0";
+  }
+  else if(Number(ip.split(".")[0]) > 223)
+  {
+    document.getElementById('decMaskTh').innerHTML = "-";
+    document.getElementById('binMaskTh').innerHTML = "-";
+  }
 }
 
 function ConvertToBinary(num)
